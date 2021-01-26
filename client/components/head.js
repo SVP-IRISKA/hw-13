9ec48bbf29e37cba011931be848e67df28abfdfe
SVP-IRISKA <63581899+SVP-IRISKA@users.noreply.github.com>
2021-01-27 01:22:15 +0300

    Update head.js

--- client/components/head.js
+++ client/components/head.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
-
+//изменения внести в этот файл
 const Head = (props) => (
   <Helmet>
     <title>SkillCrucial Boilerplate - {props.title}</title>
